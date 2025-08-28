02531f718313bb28de2c4d7fa30c5662674e5c36
Michael Fernandes <789michaelfernandes789@gmail.com>
2025-08-27 22:03:26 -0300

    atualização frequência de timer

--- src/rpi-pico.py
+++ src/rpi-pico.py
@@ -42,14 +42,14 @@ def listening(timer):
 
 
 tim = Timer()
-tim.init(freq=1, mode=Timer.PERIODIC, callback=listening)
+tim.init(freq=10, mode=Timer.PERIODIC, callback=listening)
 
 ledPin = Pin('LED', Pin.OUT)
 def led_blink(timer):
     ledPin.value((not ledPin.value()) if time_send is not None else 0)
 
 ledtim = Timer()
-ledtim.init(freq=1, mode=Timer.PERIODIC, callback=led_blink)
+ledtim.init(freq=4, mode=Timer.PERIODIC, callback=led_blink)
 
 count=0
 
